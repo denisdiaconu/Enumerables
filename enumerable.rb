fb2d77062a6d4c221eb818ca5a91c9922f6e071d
Denis Diaconu <denisdiaconu117@yahoo.com>
2021-02-12 23:21:53 +0100

    fixed

--- enumerable.rb
+++ enumerable.rb
@@ -2,14 +2,15 @@ module Enumerable
   def my_each(&block)
     return to_enum(:my_each) unless block_given?
 
+    arr = [Hash, Range].member?(self.class) ? to_a.flatten : self
     if is_a? Range
       k = 0
-      each(&block)
+      arr.each(&block)
       return self
     end
     k = 0
-    length.times do
-      yield(self[k])
+    arr.length.times do
+      yield(arr[k])
       k += 1
     end
     self
@@ -18,10 +19,11 @@ module Enumerable
   def my_each_with_index(block = nil)
     return to_enum(:my_each_with_index) unless block_given?
 
+    arr = [Hash, Range].member?(self.class) ? to_a.flatten : self
     k = 0
     if is_a? Range
       k = 0
-      each do |i|
+      arr.each do |i|
         if !block.nil?
           block.call(i, k)
         else
@@ -31,21 +33,22 @@ module Enumerable
       end
       return self
     end
-    length.times do
+    arr.length.times do
       if !block.nil?
-        block.call(self[k], k)
+        block.call(arr[k], k)
       else
-        yield(self[k], k)
+        yield(arr[k], k)
       end
       k += 1
     end
+    self
   end
 
   def my_select
     return to_enum(:my_select) unless block_given?
 
     final = []
-    num = [Hash, Range].member?(self.class) ? to_a : self
+    num = [Hash, Range].member?(self.class) ? to_a.flatten : self
     k = 0
     count do
       final << num[k] if yield(num[k])
@@ -55,39 +58,46 @@ module Enumerable
   end
 
   def my_all?(block = nil)
-    my_each do |rep|
+    my_each do |re|
       if block_given?
-        return false unless yield rep
-      elsif block.instance_of?(Class)
-        return false unless rep.instance_of?(block)
-      elsif !block.nil?
-        return false unless rep == block
+        return false unless yield re
       elsif block.instance_of?(Regexp)
-        return false unless block =~ rep
+        return false unless block =~ re
+      elsif block.instance_of?(Class)
+        return false unless re.instance_of?(block)
+      elsif !block.nil?
+        return false unless re == pattern
       else
-        return false unless rep
+        return false unless re
       end
     end
     true
   end
 
-  def my_any?(block = nil)
-    my_each do |rep|
-      if block_given?
-        return true if yield rep
-      elsif !block.nil?
-        return true if rep == block
-      elsif block.instance_of?(Class)
-        return true if rep.instance_of?(block)
-      elsif block.instance_of?(Regexp)
-        return true if block =~ rep
-      else
-        return true unless rep
+  def my_any?(match = nil)
+    i = 0
+    arr = to_a
+    stat = false
+    if block_given?
+      arr.length.times do
+        return true if yield(arr[i])
+
+        i += 1
+      end
+    elsif !match.nil?
+      arr.length.times do
+        begin
+          stat = true if arr[i].is_a?(match)
+        rescue StandardError
+          stat = true if arr[i].scan(match)
+        end
+      end
+    else
+      arr.length.times do
+        return true if arr[i] != false or !arr[i].nil?
       end
     end
-    return true if block.nil? && !block_given?
-
-    false
+    stat
   end
 
   def my_none?(block = nil)
@@ -100,6 +110,10 @@ module Enumerable
         stat = true unless yield (arr[i])
         i += 1
       end
+    elsif block.is_a? Class
+      my_each { |x| stat = true if x.class.ancestors.include?(block) }
+    elsif block.is_a? Regexp
+      my_each { |x| stat = true if block.match(x) }
     elsif !block.nil?
       arr.length.times do
         begin
@@ -110,9 +124,8 @@ module Enumerable
         i += 1
       end
     else
-      length.times do
-        return true if arr[i] == true
-        return true if arr[i] == self[i].nil?
+      arr.length.times do
+        return true if arr[i] == true or arr[i].nil?
 
         i += 1
       end
@@ -120,34 +133,34 @@ module Enumerable
     stat
   end
 
-  def my_count
-    num = 0
+  def my_count(block = nil)
+    nu = 0
     if block_given?
-      my_each { |i| num += 1 if yield(i) }
-    elsif !num.nil?
-      my_each { |i| num += 1 if i == num }
+      my_each { |i| nu += 1 if yield(i) }
+    elsif !block.nil?
+      my_each { |i| nu += 1 if i == nu }
     else
-      num = num.length
+      return size
     end
-    num
+    nu
   end
 
   def my_map(block = nil)
     arr1 = []
     arr2 = []
     x = 0
-    arr2 = if arr2 == to_a
+    arr2 = if respond_to?(:to_ary)
              self
            else
              to_a
            end
     if !block.nil?
-      arr2.my_each do
+      arr2.length.times do
         arr1.push(block.call(arr2[x]))
         x += 1
       end
     else
-      arr2.my_each do
+      arr2.length.times do
         arr1.push(yield(arr2[x]))
         x += 1
       end
@@ -155,21 +168,18 @@ module Enumerable
     arr1
   end
 
-  def my_inject(block = nil)
-    arr2 = if arr2 == to_a
-             self
-           else
-             to_a
-           end
-    if !block.nil?
-      arr2.my_inject { |addi, x| addi + x }
-    elsif block_given?
-      add = arr2[0]
-      arr2.my_each_with_index do |x, i|
-        add = yield(add, x) if i != 0
-      end
-      add
+  def my_inject(arg = nil, sym = nil)
+    if (arg.is_a?(String) || arg.is_a?(Symbol)) && (!arg.nil? && sym.nil?)
+      sym = arg
+      arg = nil
     end
+
+    if !block_given? && !sym.nil?
+      my_each { |s| arg = arg.nil? ? s : arg.send(sym, s) }
+    else
+      my_each { |s| arg = arg.nil? ? s : yield(arg, s) }
+    end
+    arg
   end
 end
 
